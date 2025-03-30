8c9e6b97dea6e3827ba81f0cf11ba52ca0fce509
flioner <lionerrochae@gmail.com>
2025-03-30 01:04:37 -0600

    textos experiencias completos, animación margin bottom

--- src/App.tsx
+++ src/App.tsx
@@ -7,111 +7,96 @@ const cardsData = [
   {
     img: "tequila.webp",
     title: "Visita Tequila",
-    desc: "Descubre Tequila, México, con un recorrido por sus icónicas destilerías...",
+    desc: "Descubre Tequila, México, con un recorrido por sus icónicas destilerías, donde podrás conocer el proceso artesanal de producción del tequila y degustar sus mejores variedades. Explora el pintoresco pueblo mágico, su historia, gastronomía y paisajes agaveros, declarados Patrimonio de la Humanidad por la UNESCO.",
     backgroundColor:
-      "linear-gradient(-30deg, rgb(159, 182, 255) 0%, rgb(105, 73, 126) 100%)",
+      "linear-gradient(-30deg, rgb(159, 182, 255) 0%, rgb(105, 73, 126) 100%)", // Estilo de fondo dinámico
     itinerary: [
       {
         title: "Tour de Fábricas",
         status: "Faltante",
-        desc: "Visita destilerías artesanales...",
+        desc: "Visita destilerías artesanales y conoce a los productores locales mientras pruebas tequilas exclusivos no disponibles en tiendas comerciales.",
       },
       {
         title: "Recorrido por el Pueblo Mágico",
         status: "Faltante",
-        desc: "Descubre las encantadoras calles...",
+        desc: "Descubre las encantadoras calles de Tequila, disfruta de su arquitectura colonial y visita sus plazas y mercados, ideales para compras artesanales.",
       },
       {
         title: "Degustación de Tequila",
         status: "Faltante",
-        desc: "Participa en una cata de tequilas premium...",
-      },
-      {
-        title: "Tour de Fábricas",
-        status: "Faltante",
-        desc: "Visita destilerías artesanales...",
-      },
-      {
-        title: "Recorrido por el Pueblo Mágico",
-        status: "Faltante",
-        desc: "Descubre las encantadoras calles...",
-      },
-      {
-        title: "Degustación de Tequila",
-        status: "Faltante",
-        desc: "Participa en una cata de tequilas premium...",
+        desc: "Participa en una cata de tequilas premium y aprende sobre sus diferentes tipos y procesos de destilación en una de las mejores casas tequileras.",
       },
     ],
   },
   {
     img: "mazamitla.webp",
     title: "Visita Mazamitla",
-    desc: "Mazamitla, conocido como el 'Pueblo Mágico' de la sierra...",
+    desc: "Mazamitla, conocido como el 'Pueblo Mágico' de la sierra, te ofrece un paisaje impresionante de bosques y montañas. Disfruta de actividades al aire libre como caminatas, paseos a caballo, y explora su pintoresco centro histórico lleno de encanto.",
     backgroundColor:
-      "linear-gradient(-30deg, rgb(105, 205, 145) 0%, rgb(27, 74, 69) 100%)",
+      "linear-gradient(-30deg, rgb(105, 205, 145) 0%, rgb(27, 74, 69) 100%)", // Otro fondo dinámico
     itinerary: [
       {
         title: "Caminata en el Bosque",
         status: "Faltante",
-        desc: "Haz una caminata guiada por los hermosos bosques...",
+        desc: "Haz una caminata guiada por los hermosos bosques de Mazamitla, rodeado de naturaleza y tranquilidad, ideal para los amantes de la naturaleza.",
       },
       {
         title: "Paseo a Caballo",
         status: "Faltante",
-        desc: "Disfruta de un paseo a caballo...",
+        desc: "Disfruta de un paseo a caballo por los paisajes de montaña, una forma única de explorar el pueblo y sus alrededores mientras aprecias la belleza natural.",
       },
       {
         title: "Visita a Cascada El Salto",
         status: "Faltante",
-        desc: "Explora la majestuosa cascada El Salto...",
+        desc: "Explora la majestuosa cascada El Salto, uno de los atractivos más populares de Mazamitla, y disfruta de un paisaje impresionante mientras te conectas con la naturaleza.",
       },
     ],
   },
   {
     img: "tapalpa.webp",
     title: "Visita Tapalpa",
-    desc: "Tapalpa es un encantador Pueblo Mágico...",
+    desc: "Tapalpa es un encantador Pueblo Mágico rodeado de montañas, ideal para quienes buscan tranquilidad y naturaleza. Aquí podrás disfrutar de sus hermosos paisajes, realizar actividades al aire libre y explorar su arquitectura tradicional.",
     backgroundColor:
-      "linear-gradient(-30deg, rgb(183, 212, 171) 0%, rgb(59, 99, 104) 100%)",
+      "linear-gradient(-30deg, rgb(183, 212, 171) 0%, rgb(59, 99, 104) 100%)", // Fondo dinámico para Tapalpa
     itinerary: [
       {
         title: "Recorrido por el Pueblo",
         status: "Faltante",
-        desc: "Pasea por las calles empedradas de Tapalpa...",
+        desc: "Pasea por las calles empedradas de Tapalpa y admira su arquitectura colonial, visitando plazas, iglesias y mercados llenos de productos artesanales.",
       },
       {
         title: "Senderismo en el Bosque",
         status: "Faltante",
-        desc: "Haz una caminata por los hermosos paisajes...",
+        desc: "Haz una caminata por los hermosos paisajes del bosque de Tapalpa, disfrutando de su flora y fauna mientras respiras aire puro.",
       },
       {
         title: "Visita al Cerro de la Cruz",
         status: "Faltante",
-        desc: "Sube al Cerro de la Cruz para obtener una vista panorámica...",
+        desc: "Sube al Cerro de la Cruz para obtener una vista panorámica de Tapalpa y sus alrededores, un lugar perfecto para disfrutar de la naturaleza.",
       },
     ],
   },
   {
     img: "sansebastian.webp",
     title: "Visita San Sebastián del Oeste",
-    desc: "San Sebastián del Oeste es un Pueblo Mágico...",
+    desc: "San Sebastián del Oeste es un Pueblo Mágico enclavado en la sierra, conocido por su rica historia minera y su entorno natural. Disfruta de su arquitectura colonial y paisajes montañosos, ideales para el ecoturismo.",
     backgroundColor:
-      "linear-gradient(-30deg, rgb(204, 163, 118) 0%, rgb(69, 56, 54) 100%)",
+      "linear-gradient(-30deg, rgb(204, 163, 118) 0%, rgb(69, 56, 54) 100%)", // Fondo dinámico para San Sebastián
     itinerary: [
       {
         title: "Recorrido Histórico",
         status: "Faltante",
-        desc: "Explora el centro histórico de San Sebastián...",
+        desc: "Explora el centro histórico de San Sebastián y conoce su historia minera, visitando museos y la iglesia principal del pueblo.",
       },
       {
         title: "Senderismo y Naturaleza",
         status: "Faltante",
-        desc: "Realiza un recorrido por los senderos del bosque...",
+        desc: "Realiza un recorrido por los senderos del bosque circundante, perfecto para quienes buscan una aventura en la naturaleza.",
       },
       {
         title: "Visita a las Minas",
         status: "Faltante",
-        desc: "Conoce las antiguas minas de San Sebastián...",
+        desc: "Conoce las antiguas minas de San Sebastián, una de las principales atracciones turísticas del pueblo que rememoran su pasado minero.",
       },
     ],
   },
@@ -147,7 +132,7 @@ function App() {
           const fullHeight = currentRef.scrollHeight;
           const adjustedHeight = fullHeight * progress;
           // Apply the current height based on animation progress
-          setItineraryHeight(adjustedHeight - 300);
+          setItineraryHeight(adjustedHeight - 200);
         }
 
         if (progress < 1) {
